5fa9104a4f623a1155b7409ac8f50c07227a5ad3
Yenchi Lin <yenchilin@gmail.com>
2022-04-02 12:56:11 -0700

    wip

--- app/src/main/java/com/cattailsw/mediaplayer/MainActivity.kt
+++ app/src/main/java/com/cattailsw/mediaplayer/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.cattailsw.mediaplayer
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -15,7 +17,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.cattailsw.mediaplayer.ui.theme.CMediaPlayerTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -23,12 +30,39 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            viewModel.state.collectLatest {
+                when(it) {
+                    MainState.Empty -> {
+                    // noop
+                    }
+                    is MainState.OpenFile -> {
+                        with(Dispatchers.Main) {
+                            startActivityForResult(it.intentToLaunch, 42)
+                        }
+                    }
+                    MainState.ErrorOpen -> {
+                        Toast.makeText(this@MainActivity, "open failed", Toast.LENGTH_SHORT).show()
+                    }
+                    is MainState.LaunchMedia -> TODO()
+                }
+            }
+        }
+
         setContent {
             MainScreen({
                 viewModel.openLocalFileBrowser()
             }, {
                 viewModel.launchMedia(Uri.parse("random string"))
             })
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 42) {
+            viewModel.handleFileOpenResult(resultCode, data)
         }
     }
 }
